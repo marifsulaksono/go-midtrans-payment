de45672bb1fb8dd15fe7cf0cd754b8cc30b463d5
marifsulaksono <marifsulaksono@gmail.com>
2023-11-17 10:05:09 +0700

    refactor:add option.ServerAPI and SetTLSConfig on database connection

--- config/database.go
+++ config/database.go
@@ -2,13 +2,16 @@ package config
 
 import (
 	"context"
+	"crypto/tls"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func Connect(conf *DBConfig) (*mongo.Client, error) {
-	db, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(conf.DatabaseURI))
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(conf.DatabaseURI).SetServerAPIOptions(serverAPI)
+	db, err := mongo.Connect(context.TODO(), opts.SetTLSConfig(&tls.Config{InsecureSkipVerify: true}))
 	if err != nil {
 		return nil, err
 	}
